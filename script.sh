80d8c7a2ca631c9597c8a16b6a61047a19498fc3
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2024-02-18 18:43:25 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,7 @@
 # Ejemplo de uso de la contraseña en un script bash
 echo "La contraseña es: $DB_PASSWORD"
+if [[ $DB_PASSWORD == "password" ]] ; then 
+    echo "Ya lo entiendo"
+else 
+    echo "no lo entiendo"
+fi
